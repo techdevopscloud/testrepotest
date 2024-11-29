f88c37f93c82afa58ae07b7c756f6b051a186d56
techdevopscloud <jaygkek@gmail.com>
2024-11-29 07:44:52 +0530

    Update cloudbuket.tf

--- cloudbuket.tf
+++ cloudbuket.tf
@@ -1,6 +1,6 @@
-/*resource "google_storage_bucket" "my_bucket" {
+resource "google_storage_bucket" "my_bucket" {
   name     = "my-unique-testtfnov28th"
   location = "US"
   project = "mygcptestproject-437406"
 
-}*/
+}
