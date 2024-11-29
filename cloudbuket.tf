178c40094b1e88c6029ae9c0f1678c99b4305136
techdevopscloud <jaygkek@gmail.com>
2024-11-29 07:46:49 +0530

    Update cloudbuket.tf

--- cloudbuket.tf
+++ cloudbuket.tf
@@ -1,3 +1,8 @@
+variable "bucketname" {
+  description = "The GCP region"
+  type        = string
+}
+
 resource "google_storage_bucket" "my_bucket" {
   name     = var.bucketname
   location = "US"
